4c276f1bb950625407f251ca56a392291c257af0
olkmphy <nam.nguyenolkmphy@hcmut.edu.vn>
2024-01-13 17:06:46 +0700

    Initial commit

---
+++ Source code/main.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "restaurant.cpp"
+
+int main(int argc, char* argv[]) {
+    string fileName = "D:\\My projects\\DSA Assignment 2\\Source code\\test.txt";
+    simulate(fileName);
+
+    return 0;
+}
